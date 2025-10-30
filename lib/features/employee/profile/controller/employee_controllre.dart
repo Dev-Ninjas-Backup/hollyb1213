aee408457e768242ca434a154713709b1872e239
Saklain <saklain.flutter@gmail.com>
2025-10-31 02:38:23 +0600

    added settings

--- lib/features/employee/profile/controller/employee_controllre.dart
+++ lib/features/employee/profile/controller/employee_controllre.dart
@@ -1,5 +1,6 @@
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:hollyb1213/core/common/constants/iconpath.dart';
+import 'package:hollyb1213/features/employee/profile/model/settings_model.dart';
 
 class EmployeeProfileControllre extends GetxController {
   final List<Map<String, dynamic>> statsList = [
@@ -23,5 +24,39 @@ class EmployeeProfileControllre extends GetxController {
       "count": "12",
       "completedMsg": "This Month",
     },
+  ];
+
+  final List<SettingsModel> settingsitems = [
+    SettingsModel(
+      imageUrl: Iconpath.profile2,
+      title: "Profile Info",
+      subTitle: "Update your name, email, or phone number",
+    ),
+    SettingsModel(
+      imageUrl: Iconpath.privacy,
+      title: "Privacy & privacy",
+      subTitle: "Manage your privacy settings",
+    ),
+
+    SettingsModel(
+      imageUrl: Iconpath.document,
+      title: "Documents",
+      subTitle: "ID verification and certificates",
+    ),
+    SettingsModel(
+      imageUrl: Iconpath.support,
+      title: "Help & Support",
+      subTitle: "Get help or contact support",
+    ),
+    SettingsModel(
+      imageUrl: Iconpath.about,
+      title: "About Us",
+      subTitle: "Get help or contact support",
+    ),
+    SettingsModel(
+      imageUrl: Iconpath.changepassword,
+      title: "Change Password",
+      subTitle: "Update your login password for security",
+    ),
   ];
 }
---
+++ lib/features/employee/profile/model/settings_model.dart
@@ -0,0 +1,12 @@
+
+class SettingsModel {
+  final String imageUrl;
+  final String title;
+  final String subTitle;
+
+  SettingsModel({
+    required this.imageUrl,
+    required this.title,
+    required this.subTitle,
+  });
+}

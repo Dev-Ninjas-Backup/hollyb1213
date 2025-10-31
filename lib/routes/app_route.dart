73c72969a2b36741eb18657163292999173fa4fa
Borhan <borhankustia@gmail.com>
2025-10-31 06:03:20 +0600

    Route Fixed

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -14,6 +14,7 @@ import 'package:hollyb1213/features/auth/verification_otp/screen/verification_sc
 import 'package:hollyb1213/features/employee/bottom_navbar/screen/employee_bottom_navbar_screen.dart';
 import 'package:hollyb1213/features/employee/home/job_details/screen/job_details_screen.dart';
 import 'package:hollyb1213/features/employee/home/screen/employe_home_screen.dart';
+import 'package:hollyb1213/features/employee/jobs/screen/employee_jobs_screen.dart';
 import 'package:hollyb1213/features/employee/profile_screen/privacy/employee_privacy.dart';
 import 'package:hollyb1213/features/onboarding/screen/onboarding_screen.dart';
 
@@ -39,9 +40,9 @@ class AppRoute {
   static String jobDetailsScreen = '/jobDetailsScreen';
 
   //profile
-  static String employeeProfileInfo="/employee/employeeProfileInfo";
-  static String employeeprivacy="/employee/privacy";
-
+  static String employeeProfileInfo = "/employee/employeeProfileInfo";
+  static String employeeprivacy = "/employee/privacy";
+  static String employeeAvailablejobs = '/employeeAvailablejobs';
 
   static String getroleSelection() => roleSelectionScreen;
   static String getonboardingScreen() => onboardingScreen;
@@ -61,9 +62,9 @@ class AppRoute {
 
   static String getjobDetailsScreen() => jobDetailsScreen;
 
-  static String getemployeeProfileInfo()=>employeeProfileInfo;
-  static String getEmployreeprivacy()=>employeeprivacy;
-
+  static String getemployeeProfileInfo() => employeeProfileInfo;
+  static String getEmployreeprivacy() => employeeprivacy;
+  static String getEmployeeAvailableJobs() => employeeAvailablejobs;
 
   static List<GetPage> routes = [
     GetPage(
@@ -147,8 +148,8 @@ class AppRoute {
       transition: Transition.fadeIn,
     ),
 
-    GetPage(name: employeeProfileInfo, page: ()=>EmployeeProfileInfoPage()),
-    GetPage(name: employeeprivacy, page: ()=>EmployeePrivacy())
-
+    GetPage(name: employeeProfileInfo, page: () => EmployeeProfileInfoPage()),
+    GetPage(name: employeeprivacy, page: () => EmployeePrivacy()),
+    GetPage(name: employeeAvailablejobs, page: () => JobScreen()),
   ];
 }

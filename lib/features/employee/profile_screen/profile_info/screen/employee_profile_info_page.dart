bac1542fc8e1cca9723c822ced5fe890be194d2d
tahmidulislamfahim <tahmidulislamfahim@gmail.com>
2026-03-12 02:06:06 +0600

    fixed profile edit

--- lib/features/employee/profile_screen/profile_info/screen/employee_profile_info_page.dart
+++ lib/features/employee/profile_screen/profile_info/screen/employee_profile_info_page.dart
@@ -7,7 +7,6 @@ import 'package:hollyb1213/core/common/constants/widget/custom_app_bar.dart';
 import 'package:hollyb1213/core/common/constants/widget/custom_button.dart';
 import 'package:hollyb1213/core/common/constants/widget/custom_text_field.dart';
 import 'package:hollyb1213/core/common/style/global_text_style.dart';
-import 'package:hollyb1213/features/auth/role_selection/screen/role_selection_screen.dart';
 import 'package:hollyb1213/features/employee/profile_screen/profile_info/controller/employee_profile_info_controller.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -19,111 +18,186 @@ class EmployeeProfileInfoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CustomAppBar(title: "Profile Info", iconUrl: Iconpath.backIcon),
-              SizedBox(height: 24.h),
-              Center(
-                child: Stack(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(61.r),
-                      child: Image.asset(
-                        Imagepath.profile,
-                        height: 122.w,
-                        width: 122.w,
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 2,
-                      right: 1.1,
-
-                      child: GestureDetector(
-                        onTap: () {
-                          controller.pickImage(ImageSource.gallery);
-                        },
-                        child: Image.asset(
-                          Iconpath.editicon,
-                          height: 40.h,
-                          width: 40.w,
+      body: Obx(
+        () => controller.isLoading.value
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomAppBar(
+                          title: "Profile Info", iconUrl: Iconpath.backIcon),
+                      SizedBox(height: 24.h),
+                      Center(
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(61.r),
+                              child: Obx(
+                                () => controller.selectedImage.value != null
+                                    ? Image.file(
+                                        controller.selectedImage.value!,
+                                        height: 122.w,
+                                        width: 122.w,
+                                        fit: BoxFit.cover,
+                                      )
+                                    : controller
+                                            .profilePhotoUrl.value.isNotEmpty
+                                        ? Image.network(
+                                            controller.profilePhotoUrl.value,
+                                            height: 122.w,
+                                            width: 122.w,
+                                            fit: BoxFit.cover,
+                                            errorBuilder:
+                                                (context, error, stackTrace) {
+                                              return Image.asset(
+                                                Imagepath.profile,
+                                                height: 122.w,
+                                                width: 122.w,
+                                              );
+                                            },
+                                          )
+                                        : Image.asset(
+                                            Imagepath.profile,
+                                            height: 122.w,
+                                            width: 122.w,
+                                          ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 2,
+                              right: 1.1,
+                              child: GestureDetector(
+                                onTap: () {
+                                  controller.pickImage(ImageSource.gallery);
+                                },
+                                child: Image.asset(
+                                  Iconpath.editicon,
+                                  height: 40.h,
+                                  width: 40.w,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 20.h),
+                      Text(
+                        "Personal Details",
+                        style: getBodyTextStyle(
+                          fontSize: sp(20),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(height: 20.h),
+                      CustomTextField(
+                        controller: controller.fullNameController,
+                        lebelText: "Full name",
+                        hintText: "Nicolas",
+                      ),
+                      CustomTextField(
+                        controller: controller.phoneNumberController,
+                        lebelText: "Phone Number",
+                        hintText: "+1 (555) 123-4567",
+                      ),
+                      CustomTextField(
+                        controller: controller.addressController,
+                        lebelText: "Address",
+                        hintText: "House #5, Dhaka Bangladesh",
+                      ),
+                      CustomTextField(
+                        controller: controller.dobController,
+                        lebelText: "Date of Birth",
+                        hintText: "12 may 1999",
+                      ),
+                      CustomTextField(
+                        controller: controller.experienceYearsController,
+                        lebelText: "Experience Years",
+                        hintText: "5",
+                      ),
+                      CustomTextField(
+                        controller: controller.skillController,
+                        lebelText: "Skills",
+                        hintText:
+                            "Cleaning, waiter, Kitchen Assistant Kitchen Helper",
+                      ),
+                      SizedBox(height: 30.h),
+                      CustomButton(
+                        buttonText: "Save Changes",
+                        onTap: () {
+                          controller.updateProfile();
+                        },
+                      ),
+                      SizedBox(height: 30.h),
+                      GestureDetector(
+                        onTap: () {
+                          _showDeleteConfirmation(context, controller);
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "assets/icons/delete.png",
+                              height: 40.h,
+                              width: 40.w,
+                            ),
+                            SizedBox(width: 8.w),
+                            Text(
+                              "Delete Account",
+                              style: getTextStyle(color: Color(0xFFFF2F2F)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 40.h),
+                    ],
+                  ),
                 ),
               ),
+      ),
+    );
+  }
 
-              SizedBox(height: 20.h),
-              Text(
-                "Personal Details",
-                style: getBodyTextStyle(
-                  fontSize: sp(20),
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              SizedBox(height: 20.h),
-
-              CustomTextField(
-                controller: controller.fullNameController,
-                lebelText: "Full name",
-                hintText: "Nicolas",
-              ),
-
-              CustomTextField(
-                controller: controller.phoneNumberController,
-                lebelText: "Phone Number",
-                hintText: "+1 (555) 123-4567",
-              ),
-              CustomTextField(
-                controller: controller.addressController,
-                lebelText: "Address",
-                hintText: "House #5, Dhaka Bangladesh",
-              ),
-              CustomTextField(
-                controller: controller.dobController,
-                lebelText: "Date of Birth",
-                hintText: "12 may 1999",
-              ),
-              CustomTextField(
-                controller: controller.skillController,
-                lebelText: "Skills",
-                hintText: "Cleaning, waiter, Kitchen Assistant Kitchen Helper",
-              ),
-
-              SizedBox(height: 30.h),
-              CustomButton(buttonText: "Save Changes", onTap: () {}),
-              SizedBox(height: 30.h),
-              GestureDetector(
-                onTap: () {
-                  Get.offAll(RoleSelectionScreen());
-                },
-
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      "assets/icons/delete.png",
-                      height: 40.h,
-                      width: 40.w,
-                    ),
-                    SizedBox(width: 8.w),
-                    Text(
-                      "Delete Account",
-                      style: getTextStyle(color: Color(0xFFFF2F2F)),
-                    ),
-                  ],
-                ),
-              ),
-
-              SizedBox(height: 40.h),
-            ],
+  void _showDeleteConfirmation(
+      BuildContext context, EmployeeProfileInfoController controller) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(
+          "Delete Account",
+          style: getBodyTextStyle(
+            fontSize: sp(18),
+            fontWeight: FontWeight.w600,
+            color: Color(0xFFFF2F2F),
           ),
         ),
+        content: Text(
+          "Are you sure you want to permanently delete your account? This action cannot be undone.",
+          style: getTextStyle(fontSize: sp(14)),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Get.back(),
+            child: Text(
+              "Cancel",
+              style: getTextStyle(color: Colors.grey),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Get.back();
+              controller.deleteProfile();
+            },
+            child: Text(
+              "Delete",
+              style: getTextStyle(color: Color(0xFFFF2F2F)),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/features/employee/profile_screen/profile_info/service/employee_profile_info_service.dart
@@ -0,0 +1,102 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+import 'package:get/get.dart';
+import 'package:hollyb1213/core/common/constants/api_endpoint.dart';
+import 'package:hollyb1213/core/common/share_preferrance/share_preferrance_helper.dart';
+
+class EmployeeProfileInfoService extends GetConnect {
+  static String _mimeType(String path) {
+    final ext = path.split('.').last.toLowerCase();
+    if (ext == 'png') return 'image/png';
+    if (ext == 'webp') return 'image/webp';
+    return 'image/jpeg';
+  }
+
+  Future<Response> getProfile() async {
+    final token = await SharedPreferenceHelper().getAccessToken();
+    final response = await get(
+      ApiEndpoint.getProfile,
+      headers: {
+        'Authorization': 'Bearer ${token ?? ''}',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    );
+    print('Profile API Response: ${response.body}');
+    return response;
+  }
+
+  Future<Response> updateProfile({
+    required String fullName,
+    required String phoneNumber,
+    required String address,
+    required String dateOfBirth,
+    required List<String> skills,
+    required int experienceYears,
+    File? profilePhoto,
+  }) async {
+    final token = await SharedPreferenceHelper().getAccessToken();
+
+    if (token == null || token.isEmpty) {
+      throw Exception('Authentication token not found. Please log in again.');
+    }
+
+    final formData = FormData({
+      'fullName': fullName,
+      'phoneNumber': phoneNumber,
+      'address': address,
+      'dateOfBirth': dateOfBirth,
+      'experienceYears': experienceYears.toString(),
+    });
+
+    // Add skills array
+    for (int i = 0; i < skills.length; i++) {
+      formData.fields.add(MapEntry('skills', skills[i]));
+    }
+
+    // Add profile photo if provided
+    if (profilePhoto != null) {
+      final bytes = await profilePhoto.readAsBytes();
+      final fileName = profilePhoto.path.split('/').last;
+      formData.files.add(
+        MapEntry(
+          'profilePhoto',
+          MultipartFile(
+            bytes,
+            filename: fileName,
+            contentType: _mimeType(fileName),
+          ),
+        ),
+      );
+    }
+
+    final response = await patch(
+      ApiEndpoint.updateEmployeeProfile,
+      formData,
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Accept': '*/*',
+      },
+    );
+
+    print('Update Profile Response Status: ${response.statusCode}');
+    print('Update Profile Response Body: ${response.body}');
+
+    return response;
+  }
+
+  Future<Response> deleteUserProfile() async {
+    final token = await SharedPreferenceHelper().getAccessToken();
+    final response = await delete(
+      ApiEndpoint.deleteUserProfile,
+      headers: {
+        'Authorization': 'Bearer ${token ?? ''}',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    );
+    print('Profile API Response: ${response.body}');
+    return response;
+  }
+}
